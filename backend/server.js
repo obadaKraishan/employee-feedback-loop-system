726d16daa27004ca448a087e7eaa45a03a1d5ee3
obada kraishan <obada.kraishan@gmail.com>
2024-08-26 02:20:05 -0500

    Backend Feedback

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use('/api/feedback', require('./routes/feedbackRoutes'));
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
 const PORT = process.env.PORT || 5001;
-
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
